9caaa9b1b6cee1158b61999379f8b4adcd6973a6
Salve Spinnangr <salve.spinnangr@altibox.no>
2021-12-12 23:07:19 +0100

    Day 12

--- day12/day12.go
+++ day12/day12.go
@@ -16,18 +16,51 @@ func Run() {
 }
 
 func part1() {
-	fmt.Printf("Part 1 answer: \n\n")
+	fmt.Printf("Part 1 answer: %d\n\n", traverse(1, nil, inputs(), "start"))
 }
 
 func part2() {
-	fmt.Printf("Part 2 answer: \n\n")
+	fmt.Printf("Part 2 answer: %d\n\n", traverse(2, nil, inputs(), "start"))
 }
 
-func inputs() (o []string) {
-	for _, line := range strings.Fields(input) {
-		for _, v := range strings.Fields(line) {
-			o = append(o, v)
+func traverse(max int, visited []string, connections map[string][]string, current string) (count int) {
+	if current == "end" {
+		return 1
+	}
+	if isLower(current) && in(visited, current) {
+		if max == 1 || current == "start" {
+			return 0
+		}
+		if max == 2 {
+			max = 1
 		}
 	}
-	return o
+	visited = append(visited, current)
+	for _, c := range connections[current] {
+		count += traverse(max, visited, connections, c)
+	}
+	return count
+}
+
+func in(l []string, s string) bool {
+	for _, v := range l {
+		if v == s {
+			return true
+		}
+	}
+	return false
+}
+
+func isLower(s string) bool {
+	return strings.ToLower(s) == s
+}
+
+func inputs() map[string][]string {
+	connections := make(map[string][]string)
+	for _, line := range strings.Fields(input) {
+		cave := strings.Split(line, "-")
+		connections[cave[0]] = append(connections[cave[0]], cave[1])
+		connections[cave[1]] = append(connections[cave[1]], cave[0])
+	}
+	return connections
 }
